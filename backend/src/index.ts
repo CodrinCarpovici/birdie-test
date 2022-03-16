a466d49153b94870d523c7d167bfd20cd4c213b6
Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2022-03-16 12:51:21 +0000

    heroku config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,25 @@
+import * as express from "express";
+import path = require("path");
 import app from './application';
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "../../frontend")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+});
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`);
 });
+
